48e27ffd0eb719a234ce713a45c2afdf7b7971ce
VarunaLex <lex.varuna@gmail.com>
2018-11-22 21:41:39 +0530

    initi index.js

---
+++ src/index.js
@@ -0,0 +1,4 @@
+console.warn('I am a Star Trek nerd');
+console.log('So through out this tutorial, you will see a lot of Star Trek quotes');
+console.log('Starting now');
+console.log("Compassion: that’s the one thing no machine ever had. Maybe it’s the one thing that keeps men ahead of them. -Dr McCoy");
