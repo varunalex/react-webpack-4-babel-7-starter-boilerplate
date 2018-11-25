0563f18e439b6b76faa3ba195f4ea65a936832dc
VarunaLex <lex.varuna@gmail.com>
2018-11-25 13:00:32 +0530

    test build

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Jsx from './components/jsx.jsx'
+import Jsx from './components/jsx.jsx';
 
 import './styles.scss';
 
-const App = () => {
-  return (
+const App = () => (
     <div className="app">
     <img alt="header" src="/dist/images/header.png" className="app-header" />
       <h2>Yoo! Start to build your app.</h2>
       <Jsx />
     </div>
-  );
-};
+);
 
 ReactDOM.render(<App />, document.getElementById('app'));
 
